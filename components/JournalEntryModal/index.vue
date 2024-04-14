a632e1cfa36719f4ef165e6aee3c28e261f697a5
connordeaneuz <connordeane@urbanzoo.io>
2024-04-14 15:50:24 +0100

    modal styling

--- components/JournalEntryModal/index.vue
+++ components/JournalEntryModal/index.vue
@@ -3,17 +3,31 @@
     <div class="fixed inset-0 z-[100] bg-black/75"></div>
 
     <div
-      class="fixed z-[1000] top-5 left-5 right-5 bottom-5 lg:top-[150px] lg:left-[150px] lg:right-[150px] lg:bottom-[150px] bg-white rounded-xl shadow-2xl"
+      class="fixed overflow-hidden py-12 px-8 flex flex-col justify-end z-[1000] top-5 left-5 right-5 bottom-5 lg:top-[150px] lg:left-[150px] lg:right-[150px] lg:bottom-[150px] bg-white rounded-xl shadow-2xl"
     >
       <button
         @click="closeModal"
-        class="absolute right-4 top-4 bg-slate-400 h-8 w-8 rounded-full text-white"
+        class="absolute right-4 top-4 bg-red-400 h-8 w-8 rounded-full text-white"
       >
         X
       </button>
 
-      <input type="text" v-model="newMessage" />
-      <button @click="insertData(newMessage)">post</button>
+      <div class="bg-red-500 flex justify-center h-full">
+        <textarea
+          placeholder="What's on your mind?..."
+          class="h-full w-full focus:outline-none resize-none"
+          type="text"
+          v-model="newMessage"
+        />
+      </div>
+      <div class="w-full flex justify-end">
+        <button
+          class="bg-green-400 px-8 py-2 rounded-3xl uppercase font-bold text-white shadow-lg"
+          @click="insertData(newMessage)"
+        >
+          post
+        </button>
+      </div>
     </div>
   </section>
 </template>
